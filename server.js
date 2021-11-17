e8cf62eb0093f6aeced4ef9aebbc151ae571b20c
Martin <martin.speedie@appscore.com.au>
2021-11-17 15:48:52 +1100

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var cors_proxy = require("./lib/cors-anywhere");
 cors_proxy
   .createServer({
     originWhitelist: [], // Allow all origins
-    requireHeader: ["origin", "x-requested-with"],
+    // requireHeader: ["origin", "x-requested-with"],
     removeHeaders: ["cookie", "cookie2"],
   })
   .listen(port, host, function () {
